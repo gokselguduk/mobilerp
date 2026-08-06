ea324628bde75a3aa588d1da23d9fbdad085505c
gokselguduk <gokselguduk@gmail.com>
2026-08-06 14:00:19 +0300

    Delete MOBIL-APK-HAZIRLA.bat

--- MOBIL-APK-HAZIRLA.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-REM Eski ad → Android betigine yonlendir
-echo MOBIL-APK-HAZIRLA.bat artik MOBIL-ANDROID-HAZIRLA.bat olarak ayrildi.
-echo.
-call "%~dp0MOBIL-ANDROID-HAZIRLA.bat"
